2f99064943be825a3902b924360b7e74e58db4c4
nagasatish <nagasatish54@gmail.com>
2019-03-21 10:26:52 +0000

    mywife

---
+++ venu.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+<TITLE></TITLE>
+<BODY TOPMARGIN="0" LEFTMARGIN="0" BACKGROUND="e:\WALLPAPERS\LCE\BG.JPG">
+<LINK
+  MEDIA=SCREEN A HREF="e:/WALLPAPERS/lce/STYLE.CSS" TYPE=TEXT/CSS REL=STYLESHEET><STYLE TYPE=TYPE/TEXT>  </STYLE></head>
+<IMG SRC="e:\WALLPAPERS\LCE\HEAD1.GIF">
+<table cellspacing="0" cellpadding="4"  border="0" id="table" width="100%">
+<TR valign="left">
+<td width="70%">
+<p align="left">
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\Alenora.HTML">lenora</A>|
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\A.HTML">collage</A>|
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\A.HTML">fauculty</A>|
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\satish.HTML">Infrastructure</A>|
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\star.HTML">administration</A>|
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\sat.HTML">features & placementdesk</A>|
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\A.HTML">transport</A>|
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\A.HTML">Downloads</A>
+</td>
+<td valign="top" width="30%">
+<p align="right">
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\nn.HTML"><img src="e:/wallpapers/lce/home_icon.gif">Home|
+<A HREF="C:\DOCUMENTS AND SETTINGS\nagasatish\MY DOCUMENTS\A.HTML"><img src="e:/wallpapers/lce/contact_us.gif">contaous
+</p>
+</td>
+</table>
+<table cellspacing="0" cellpadding="4"  border="0" id="table" width="100%">
+<tr>
+<td valign="top" width="50%">
+<p class="subheading_style">special features</p>
+<marquee  onmouseover=stop() onmouseout=start() scrollamount="3" direction=up loop=true height="260">
+<p align="left"><p class=run_text><h4><font color="maroon"> Health care :<font color="black"> The campuss is equipped with and medical officer as well as lady 
+doctor. An ambulance is &nbsp  available on the campus for any emergency<br><br><br><font color="maroon">
+sports and games: <font color="black">The lenora has a gymnasium with sophicicated aquipment and also facilities 
+indoor and  &nbsp games<br><br> <br><font color="maroon">Night Study Hours : <font color="black">Hostel students have been given Night study Classes every day under the supervision 
+ &nbsp  of faculty<br><br><br>
+<font color="maroon">Alumuni Allocation :<font color="black"> Lenora Alumni are the source of strength to this college.Our AlumniAre spread all over 
+ &nbsp   World 
+</p></marquee>  
+<td valign="top" width="5%"></td>
+<td valign="top" width="45%">
+<p class="subheading_style">Training and Placement Desk</p>
+<p align="top"><h4><font color="maroon"><b>&nbsp&nbsp&nbsp&nbsp&nbsp The Training and Placement Desk was established along with the college it has been providing assistance to the students in getting placements
+in various organisations  the active placement Desk conducts carrer Guidance programmes for pre-final and Final Year students.
+<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspThese activities have proved exceptionally useful in shaping the students'carreer.This Taining and placement Desk 
+have been maintaining a close liaison witha large number of private/public with a view to provide,In-house Training and placement Services.</p> <br><br>  <bs>
+</td>
+</tr>
+</table><p align="right"></p>
+<br>
+<br>
+
+
+<div class=fotter_img style=text align="center">
+<p class=fotter_text>
+<a href="c:/documents and settings/nagasatish/my documents/a.html">Home</a>|<a
+href="e:/wallpapers/lce/a.html">contauous</a>
+<span class=fotter_text>
+<P ALIGN="center">Copyright © 2010 Lenora College of Engineering 
+| All rights reserved | Reproduction in whole or in part without permission is 
+prohibited.</SPAN><BR><SPAN class=footer_text1>This site is best viewed by 
+Internet Explorer 6 and above versions with 1024 * 768 Pixels and Firefox 2.0.1 
+and above versions</p>  </span> </div>
+</body>
+</html>
